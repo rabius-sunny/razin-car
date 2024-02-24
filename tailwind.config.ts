89ad89c0fce48f1d23ff10890cfd15303c819947
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-02-24 22:59:23 +0600

    update: tailwind colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,41 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+  darkMode: ['class'],
+  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    colors: {
+      primary: '#E11D48',
+      secondary: '#4F46E5',
+      success: '#34D399',
+      black: '#282828',
+      white: colors.white,
+      red: colors.red,
+      rose: colors.rose,
+      slate: colors.slate,
+      transparent: colors.transparent,
+      green: colors.green
     },
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')]
 } satisfies Config
 
 export default config
