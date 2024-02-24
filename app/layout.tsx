82c64fb25dc44675866586e14b798c080423e858
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-02-24 22:27:13 +0600

    add: site config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
+import { siteInfo } from '~/configs/site'
+
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Razin Car',
-  description: 'Car decoration accessories that you ever wanted.'
+  title: {
+    default: siteInfo.name,
+    template: `%s | ${siteInfo.name}`
+  },
+  description: siteInfo.description,
+  authors: siteInfo.authors,
+  generator: siteInfo.generator
 }
 
 export default function RootLayout({
@@ -16,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   )
 }
---
+++ configs/site.ts
@@ -0,0 +1,17 @@
+export const siteInfo = {
+  name: 'Razin Car',
+  description: 'Car decoration accessories that you ever wanted.',
+  authors: {
+    url: 'https://github.com/rabius-sunny',
+    name: 'Rabius Sunny'
+  },
+  generator:
+    'car decoration accessories headlight backlight taillight perfume scent car body parts car-parts denting painting',
+  navItems: [
+    { title: 'Home', href: '/' },
+    { title: 'About Us', href: '/' },
+    { title: 'Products', href: '/' },
+    { title: 'Blogs', href: '/' },
+    { title: 'Contact', href: '/' }
+  ]
+}
